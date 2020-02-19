ee4460f9fb97b80dfe556e633ac4665ded72ef67
方方方 <a1120309644@qq.com>
2020-02-19 14:47:38 +0800

    初始化-整合路由

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入路由
+import router from './index.js'
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  // 挂载到Vue实例上
+  router
+  // 挂载到id为app的div上
 }).$mount('#app')
